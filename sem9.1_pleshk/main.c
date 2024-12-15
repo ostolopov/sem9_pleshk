4a6be3d5688855c81f05a29ed4a8143e22a8b499
ostolopov <egor21231bar@gmail.com>
2024-12-16 00:27:55 +0300

    Initial Commit

---
+++ sem9.1_pleshk/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sem9.1_pleshk
+//
+//  Created by Egor Baranov on 16.12.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

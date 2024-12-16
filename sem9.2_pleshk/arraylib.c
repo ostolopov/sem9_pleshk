f1db4152df6084ddcad5b2da3d5e6cf967f78dd8
ostolopov <egor21231bar@gmail.com>
2024-12-16 23:37:09 +0300

    final

---
+++ sem9.2_pleshk/arraylib.c
@@ -0,0 +1,8 @@
+//
+//  arraylib.c
+//  sem9.2_pleshk
+//
+//  Created by Egor Baranov on 16.12.2024.
+//
+
+#include "arraylib.h"
---
+++ sem9.2_pleshk/arraylib.h
@@ -0,0 +1,13 @@
+//
+//  arraylib.h
+//  sem9.2_pleshk
+//
+//  Created by Egor Baranov on 16.12.2024.
+//
+
+#ifndef arraylib_h
+#define arraylib_h
+
+#include <stdio.h>
+
+#endif /* arraylib_h */
